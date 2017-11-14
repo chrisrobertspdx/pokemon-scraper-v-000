309a8438d899d178cee0419d92b9d0391566d744
Chris Roberts <chrisrobertspdx@gmail.com>
2017-11-14 12:00:22 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,20 @@
 class Pokemon
+  attr_accessor :id, :name, :type, :db, :hp
+  @@all = []
+  def initialize(id:,name:,type:,db:)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+    @hp = 60
+  end
+  def self.save(name,type,db)
+    db.execute("INSERT INTO pokemon (name,type) VALUES (?, ?)",name,type)
+  end
+  def self.find(id,db)
+    record=db.execute("select * from pokemon where id = (?)",id)
+    Pokemon.new(id: record[0][0],name: record[0][1], type: record[0][2], db: db)
+  end
+
+
 end
